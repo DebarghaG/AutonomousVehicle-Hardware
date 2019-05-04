f6066d13c292e9de7cd4734000369c86d95b44a8
Debargha Ganguly <royal.debargha@gmail.com>
2019-05-05 01:08:06 +0530

    Adding pintool to the benchmarking suite

---
+++ ComputerVision-Benchmarking/CPU-Programs/test.sh
@@ -0,0 +1,1 @@
+/home/debargha/Downloads/pin-3.7-97619-g0d0c92f4f-gcc-linux/pin -t /home/debargha/Downloads/pin-3.7-97619-g0d0c92f4f-gcc-linux/source/tools/SimpleExamples/obj-intel64/opcodemix.so -- /home/debargha/Documents/AutonomousVehicle-Hardware/ComputerVision-Benchmarking/CPU-Programs/houghlines
